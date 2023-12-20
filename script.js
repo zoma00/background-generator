f286c479a8bb7244f0c1e7da82eb6f5767db364a
zoma00 <zoma0097@gmail.com>
2023-12-20 22:41:33 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Your JavaScript code goes here
+  // For example:
+  // Select elements, add event listeners, and define functions
+});
